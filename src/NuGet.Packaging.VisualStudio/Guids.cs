c5af31f6acf8376da1370b12cba6a6bd86f6d8b6
Adrian Alonso <alo@xamarin.com>
2016-07-01 14:21:15 -0300

    Added "empty" project system for VS based on NuProj

---
+++ src/NuGet.Packaging.VisualStudio/Guids.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet.Packaging.VisualStudio
+{
+	internal class Guids
+	{
+		public const string PackageGuid = "e57e2daf-a455-45db-962e-31b1c9c70860";
+		public const string ProjectTypeGuid = "5dd5e4fa-cb73-4610-85ab-557b54e96aa9";
+	}
+}
---
+++ src/NuGet.Packaging.VisualStudio/VsPackage.cs
@@ -0,0 +1,12 @@
+﻿namespace NuGet.Packaging.VisualStudio
+{
+	using System;
+	using System.Runtime.InteropServices;
+	using Microsoft.VisualStudio.Shell;
+
+	[Guid(Guids.PackageGuid)]
+	[PackageRegistration(UseManagedResourcesOnly = true)]
+	public sealed class VsPackage : Package
+	{
+	}
+}
