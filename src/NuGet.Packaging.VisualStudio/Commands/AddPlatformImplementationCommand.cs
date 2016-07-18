53395f7d49f6aa8354d5867a55c5b756362d7ec8
Adrian Alonso <alo@xamarin.com>
2016-07-18 16:39:38 -0300

    Added "Add Platform Implementation" command
    (with an empty implementation for now)

---
+++ src/NuGet.Packaging.VisualStudio/Commands/AddPlatformImplementationCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+
+namespace NuGet.Packaging.VisualStudio
+{
+	[Export(typeof(DynamicCommand))]
+	class AddPlatformImplementationCommand : DynamicCommand
+	{
+		readonly ISelectionService selectionService;
+		readonly IDialogService dialogService;
+
+		[ImportingConstructor]
+		public AddPlatformImplementationCommand(ISelectionService selectionService, IDialogService dialogService)
+			: base(Commands.AddPlatformImplementationCommandId)
+		{
+			this.selectionService = selectionService;
+			this.dialogService = dialogService;
+		}
+
+		protected override void Execute()
+		{
+		}
+	}
+}
