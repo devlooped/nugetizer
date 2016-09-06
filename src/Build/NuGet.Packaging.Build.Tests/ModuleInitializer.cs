a6671ff1b9c83bb76db9bde7fca2653c8dac6f76
Daniel Cazzulino <daniel@cazzulino.com>
2016-09-06 18:01:45 -0300

    Start clean with targets, rather than bringing in existing NuProj (#19)
    This will make it far easier to end up with exactly what we need,
    and should result in an easier to maintain codebase.
    
    Reorganized the VisualStudio extension in a subfolder too for better
    layout and separation with Build and (eventually?) XamarinStudio.

---
+++ src/Build/NuGet.Packaging.Build.Tests/ModuleInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+internal static class ModuleInitializer
+{
+	public static string BaseDirectory { get; private set; }
+
+	public static void Initialize()
+	{
+		BaseDirectory = Directory.GetCurrentDirectory();
+	}
+}
---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿#pragma warning disable 0436
+
+using System.Reflection;
+using Microsoft.NuGet.Packaging;
+
+[assembly: AssemblyCompany ("Microsoft")]
+[assembly: AssemblyProduct ("Microsoft NuGet Packaging")]
+[assembly: AssemblyCopyright ("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle(ThisAssembly.AssemblyName)]
+
+
+#if DEBUG
+[assembly: AssemblyConfiguration ("DEBUG")]
+#else
+[assembly: AssemblyConfiguration ("RELEASE")]
+#endif
+
+[assembly: AssemblyVersion(ThisAssembly.SimpleVersion)]
+[assembly: AssemblyFileVersion(ThisAssembly.FullVersion)]
+[assembly: AssemblyInformationalVersion(ThisAssembly.InformationalVersion)]
+
+namespace Microsoft.NuGet.Packaging
+{
+	partial class ThisAssembly
+	{
+		/// <summary>
+		/// Simple release-like version number, with just major, minor and ending up in '0'.
+		/// </summary>
+		public const string SimpleVersion = Git.SemVer.Major + "." + Git.SemVer.Minor + ".0";
+
+		/// <summary>
+		/// Full version, including commits since base version file, like 4.0.598
+		/// </summary>
+		public const string FullVersion = SimpleVersion + "." + Git.SemVer.Patch;
+		
+		/// <summary>
+		/// Full version, plus branch and commit short sha.
+		/// </summary>
+		public const string InformationalVersion = FullVersion + "-" + Git.Branch + "+" + Git.Commit;
+    }
+}
+
+#pragma warning restore 0436
