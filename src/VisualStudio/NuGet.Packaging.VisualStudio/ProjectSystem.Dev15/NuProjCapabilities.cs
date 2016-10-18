cc025544ae23b917518015dbd07b8b5e09ca2d16
Adrian Alonso <alo@xamarin.com>
2016-10-18 14:44:04 -0300

    Fixed capability value to match the one used by the authoring targets

--- src/VisualStudio/NuGet.Packaging.VisualStudio/ProjectSystem.Dev15/NuProjCapabilities.cs
+++ src/VisualStudio/NuGet.Packaging.VisualStudio/ProjectSystem.Dev15/NuProjCapabilities.cs
@@ -6,7 +6,7 @@ namespace NuGet.Packaging.VisualStudio
 {
     internal static class NuProjCapabilities
     {
-        public const string NuProj = "NuGet.Packaging";
+        public const string NuProj = "PackagingProject";
 
         public static readonly ImmutableHashSet<string> ProjectSystem = Empty.CapabilitiesSet.Union(new[]
         {
