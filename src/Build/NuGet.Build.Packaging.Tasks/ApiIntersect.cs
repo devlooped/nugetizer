f3a064050041f1d4a29701b69388c5f8056e4d2b
Matt Ward <matt.ward@xamarin.com>
2016-10-26 13:18:34 +0100

    Support generating reference assemblies
    Adding a ReferenceAssemblyFramework item to a NuGet packaging project
    will now generate reference assemblies for the specified Portable
    Class Library profile for the referenced projects.
    
    <ReferenceAssemblyFramework Include=".NETPortable,Version=v4.5,Profile=Profile259" />

---
+++ src/Build/NuGet.Build.Packaging.Tasks/ApiIntersect.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Common;
+using NuGet.Frameworks;
+
+namespace NuGet.Build.Packaging.Tasks
+{
+	public class ApiIntersect : ToolTask
+	{
+		[Required]
+		public ITaskItem Framework { get; set; }
+
+		[Required]
+		public ITaskItem[] IntersectionAssembly { get; set; }
+
+		[Required]
+		public ITaskItem RootOutputDirectory { get; set; }
+
+		[Output]
+		public ITaskItem OutputDirectory { get; set; }
+
+		protected override string ToolName
+		{
+			get { return "ApiIntersect"; }
+		}
+
+		protected override string GenerateFullPathToTool()
+		{
+			return Path.Combine(ToolPath, ToolExe);
+		}
+
+		protected override MessageImportance StandardErrorLoggingImportance
+		{
+			get { return MessageImportance.High; }
+		}
+
+		protected override string GenerateCommandLineCommands()
+		{
+			var builder = new CommandLineBuilder();
+
+			var nugetFramework = NuGetFramework.Parse(Framework.ItemSpec);
+
+			builder.AppendSwitch("-o");
+			CreateOutputDirectoryItem(nugetFramework);
+			builder.AppendFileNameIfNotNull(OutputDirectory.ItemSpec);
+
+			string frameworkPath = GetFrameworkPath(nugetFramework);
+			builder.AppendSwitch("-f");
+			builder.AppendFileNameIfNotNull(frameworkPath);
+
+			foreach (var assembly in IntersectionAssembly.NullAsEmpty())
+			{
+				builder.AppendSwitch("-i");
+				builder.AppendFileNameIfNotNull(assembly.ItemSpec);
+			}
+
+			return builder.ToString();
+		}
+
+		void CreateOutputDirectoryItem(NuGetFramework framework)
+		{
+			string fullOutputPath = Path.Combine(RootOutputDirectory.ItemSpec, GetOutputDirectoryName(framework));
+			OutputDirectory = new TaskItem(fullOutputPath + Path.DirectorySeparatorChar);
+			OutputDirectory.SetMetadata("TargetFrameworkVersion", "v" + framework.Version.ToString(2));
+			OutputDirectory.SetMetadata("TargetFrameworkProfile", framework.Profile);
+			OutputDirectory.SetMetadata(MetadataName.TargetFrameworkMoniker, Framework.ItemSpec);
+		}
+
+		string GetFrameworkPath(NuGetFramework framework)
+		{
+			if (framework.IsPCL)
+			{
+				return GetPortableLibraryPath(framework);
+			}
+
+			throw new NotImplementedException("Only PCL profiles supported.");
+		}
+
+		string GetPortableLibraryPath(NuGetFramework framework)
+		{
+			return Path.Combine(
+				GetPortableRootDirectory(),
+				"v" + framework.Version.ToString(2),
+				"Profile",
+				framework.Profile);
+		}
+
+		static string GetPortableRootDirectory()
+		{
+			if (RuntimeEnvironmentHelper.IsMono)
+			{
+				string macPath = "/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild-frameworks/.NETPortable/";
+				if (Directory.Exists(macPath))
+					return macPath;
+
+				return Path.Combine(Path.GetDirectoryName(typeof(Object).Assembly.Location), "../xbuild-frameworks/.NETPortable/");
+			}
+
+			return Path.Combine(
+				Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86, Environment.SpecialFolderOption.DoNotVerify),
+				@"Reference Assemblies\Microsoft\Framework\.NETPortable");
+		}
+
+		string GetOutputDirectoryName(NuGetFramework framework)
+		{
+			if (framework.IsPCL && framework.HasProfile)
+			{
+				return framework.Profile;
+			}
+
+			return framework.GetShortFolderName();
+		}
+	}
+}
---
+++ src/Build/NuGet.Build.Packaging.Tasks/GenerateAssemblyInfo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace NuGet.Build.Packaging.Tasks
+{
+	public class GenerateAssemblyInfo : Task
+	{
+		[Required]
+		public ITaskItem[] Assemblies { get; set; }
+
+		[Required]
+		public ITaskItem[] OutputDirectories { get; set; }
+
+		[Output]
+		public string AssemblyName { get; set; }
+
+		public override bool Execute()
+		{
+			AssemblyName name = GetAssemblyName();
+			CreateAssemblyInfo(name);
+
+			AssemblyName = name.Name;
+
+			return true;
+		}
+
+		AssemblyName GetAssemblyName()
+		{
+			AssemblyName[] assemblyNames = Assemblies.NullAsEmpty()
+				.Select(assembly => System.Reflection.AssemblyName.GetAssemblyName(assembly.ItemSpec))
+				.ToArray();
+
+			string[] allNames = assemblyNames.Select(name => name.Name).ToArray();
+			if (allNames.Distinct().Count() > 1)
+			{
+				Log.LogWarning("Assembly names should be the same for a bait and switch NuGet package. Names: '{0}' Assemblies: {1}",
+					string.Join(", ", allNames),
+					string.Join(", ", Assemblies.Select(assembly => assembly.ItemSpec)));
+			}
+
+			Version[] allVersions = assemblyNames.Select(name => name.Version).ToArray();
+			if (allVersions.Distinct().Count() > 1)
+			{
+				Log.LogWarning("Assembly versions should be the same for a bait and switch NuGet package. Versions: '{0}' Assemblies: {1}",
+					string.Join(", ", allVersions.Select(version => version.ToString())),
+					string.Join(", ", Assemblies.Select(assembly => assembly.ItemSpec)));
+			}
+
+			return assemblyNames.FirstOrDefault();
+		}
+
+		void CreateAssemblyInfo(AssemblyName assemblyName)
+		{
+			string text = "using System.Reflection;" + Environment.NewLine +
+				string.Format("[assembly: AssemblyVersion (\"{0}\")]", assemblyName.Version);
+
+			foreach (ITaskItem directory in OutputDirectories.NullAsEmpty())
+			{
+				string fileName = Path.Combine(directory.ItemSpec, "AssemblyInfo.cs");
+				File.WriteAllText(fileName, text);
+			}
+		}
+	}
+}
