1886901a33d8815efda828caa34ce2e326afde08
Daniel Cazzulino <daniel@cazzulino.com>
2016-12-21 13:32:40 -0300

    Support simplified authoring projects
    By moving some target dependencies under the condition that IncludeOutputsInPackage
    is true, we allow simple .proj files without the MS.Common targets to also participate
    in nuget packing with minimal configuration, namely setting IncludeOutputsInPackage=false
    and IncludeFrameworkReferencesInPackage=true.

---
+++ src/Build/NuGet.Build.Packaging.Tests/given_a_custom_build_project.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.Build.Execution;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NuGet.Build.Packaging
+{
+	public class given_a_custom_build_project
+	{
+		ITestOutputHelper output;
+
+		public given_a_custom_build_project(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+		[Fact]
+		public void when_packing_then_can_include_content()
+		{
+			var result = Builder.BuildScenario(nameof(given_a_custom_build_project));
+
+			Assert.Equal(TargetResultCode.Success, result.ResultCode);
+
+			Assert.Contains(result.Items, item => item.Matches(new
+			{
+				PackagePath = "Readme.txt",
+			}));
+		}
+	}
+}
