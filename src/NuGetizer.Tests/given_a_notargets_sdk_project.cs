568b3767df82301bb396cb1374058d11af023d03
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-08 01:42:12 -0300

    Add support for Microsoft.Build.NoTargets SDK
    This allows using that SDK for packaging projects if desired. This change allows referencing such projects from other packaging projects, and also from regular Microsoft.NET.Sdk projects too.

---
+++ src/NuGetizer.Tests/given_a_notargets_sdk_project.cs
@@ -0,0 +1,108 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace NuGetizer
+{
+    public class given_a_notargets_sdk_project
+    {
+        ITestOutputHelper output;
+
+        public given_a_notargets_sdk_project(ITestOutputHelper output) => this.output = output;
+
+        [Fact]
+        public void cam_reference_packaging_project()
+        {
+            var result = Builder.BuildProjects(
+                "GetPackageContents", output, null,
+                ("main.msbuildproj", @"
+<Project Sdk='Microsoft.Build.NoTargets/2.0.1'>
+  <PropertyGroup>
+    <PackageId>Foo</PackageId>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+  <ItemGroup>
+    <PackageReference Include='Newtonsoft.Json' Version='12.0.3' />
+    <ProjectReference Include='other.msbuildproj' />
+  </ItemGroup>
+</Project>"), 
+                ("other.msbuildproj", @"
+<Project Sdk='Microsoft.Build.NoTargets/2.0.1'>
+  <PropertyGroup>
+    <PackageId>Bar</PackageId>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+  <ItemGroup>
+    <PackageReference Include='Castle.Core' Version='4.4.1' />
+  </ItemGroup>
+</Project>"));
+
+            result.AssertSuccess(output);
+            Assert.Contains(result.Items, item => item.Matches(new
+            {
+                Identity = "Foo",
+                PackFolder = PackFolderKind.Metadata
+            }));
+            Assert.Contains(result.Items, item => item.Matches(new
+            {
+                Identity = "Newtonsoft.Json",
+                PackFolder = PackFolderKind.Dependency
+            }));
+            Assert.Contains(result.Items, item => item.Matches(new
+            {
+                Identity = "Bar",
+                PackFolder = PackFolderKind.Dependency
+            }));
+            Assert.DoesNotContain(result.Items, item => item.Matches(new
+            {
+                Identity = "Castle.Core",
+                PackageId = "Foo",
+                PackFolder = PackFolderKind.Dependency
+            }));
+        }
+
+        [Fact]
+        public void cam_be_referenced()
+        {
+            var result = Builder.BuildProjects(
+                "GetPackageContents", output, null,
+                ("main.csproj", @"
+<Project Sdk='Microsoft.NET.Sdk'>
+  <PropertyGroup>
+    <PackageId>Foo</PackageId>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+  <ItemGroup>
+    <ProjectReference Include='other.msbuildproj' />
+  </ItemGroup>
+</Project>"),
+                ("other.msbuildproj", @"
+<Project Sdk='Microsoft.Build.NoTargets/2.0.1'>
+  <PropertyGroup>
+    <PackageId>Bar</PackageId>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+  <ItemGroup>
+    <PackageReference Include='Castle.Core' Version='4.4.1' />
+  </ItemGroup>
+</Project>"));
+
+            result.AssertSuccess(output);
+            Assert.Contains(result.Items, item => item.Matches(new
+            {
+                Identity = "Foo",
+                PackFolder = PackFolderKind.Metadata
+            }));
+            Assert.Contains(result.Items, item => item.Matches(new
+            {
+                Identity = "Bar",
+                PackFolder = PackFolderKind.Dependency
+            }));
+            Assert.DoesNotContain(result.Items, item => item.Matches(new
+            {
+                Identity = "Castle.Core",
+                PackageId = "Foo",
+                PackFolder = PackFolderKind.Dependency
+            }));
+        }
+    }
+}
