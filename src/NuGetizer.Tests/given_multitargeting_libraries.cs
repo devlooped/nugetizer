dae123f33a4da2a0ba92e2c43a1305c006b75e76
Daniel Cazzulino <daniel@cazzulino.com>
2020-12-04 01:42:20 -0300

    When resolving P2P references for packaging, set target framework
    When MSBuild project references are resolved, in multi-targeting P2P references, the `SetTargetFramework` metadata is populated with the nearest target framework supported by the referenced project (from the referencing project's point of view). For example, if a `netcore3.1` project references a `net5.0;netstandard2.1` one, when resolving the contents for the referenced project, the built-in targets will properly calculate (using the NuGet-provided GetReferenceNearestTargetFrameworkTask task) the target framework compatible with the referencing project.
    
    If we don't set the target framework, we end up inadvertently invoking targets on the project reference using the *calling* project target framework (in this case `netcoreapp3.1`), which is not supported by the referenced project and we end up with a missing MSBuild target import (since NuGet conditions the .props/.targets imports to the target framework), and it ends up manifesting itself as an error that the reference hasn't been nugetized, when in fact it's because we're invoking with the wrong TF.
    
    Fixes #27.

---
+++ src/NuGetizer.Tests/given_multitargeting_libraries.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NuGetizer
+{
+    public class given_multitargeting_libraries
+    {
+        ITestOutputHelper output;
+
+        public given_multitargeting_libraries(ITestOutputHelper output) => this.output = output;
+
+        [InlineData("common.csproj")]
+        [InlineData("uilibrary.csproj")]
+        [InlineData("uishared.csproj")]
+        [Theory]
+        public void when_packing_on_build_then_succeeds(string projectName)
+        {
+            Builder.BuildScenario(
+                nameof(given_multitargeting_libraries),
+                projectName: projectName, target: "Restore", output: output)
+                .AssertSuccess(output);
+
+            Builder.BuildScenario(
+                nameof(given_multitargeting_libraries),
+                projectName: projectName, target: "Build", output: output)
+                .AssertSuccess(output);
+
+            var result = Builder.BuildScenario(
+                nameof(given_multitargeting_libraries),
+                projectName: projectName,
+                target: "GetPackageTargetPath", output: output);
+
+            result.AssertSuccess(output);
+
+            Assert.Single(result.Items);
+
+            Assert.True(File.Exists(result.Items[0].GetMetadata("FullPath")));
+        }
+    }
+}
