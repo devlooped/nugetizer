09b4534f7bf7e95539421a0f41806a4b513e5932
Daniel Cazzulino <daniel@cazzulino.com>
2021-07-19 18:03:36 -0300

    Properly evaluate PackInference.PackExclude
    We were previously assuming Exclude was a valuated while doing the Include for entire item groups, while this is not the case. We need instead to evaluate using the same MSBuild syntax (we use minimatch for this) so we can properly exclude items.
    
    Fixes #128 and #122.

---
+++ src/NuGetizer.Tasks/EvaluateWildcards.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Minimatch;
+
+namespace NuGetizer.Tasks
+{
+    /// <summary>
+    /// Evaluates one or more minimatch expressions against a set of 
+    /// items and returns two lists: those that matched and those that 
+    /// didn't.
+    /// </summary>
+    public class EvaluateWildcards : Task
+    {
+        [Required]
+        public ITaskItem[] Items { get; set; }
+
+        [Required]
+        public string Wildcards { get; set; }
+
+        [Output]
+        public ITaskItem[] MatchingItems { get; set; }
+
+        [Output]
+        public ITaskItem[] NonMatchingItems { get; set; }
+
+        public override bool Execute()
+        {
+            var matching = new List<ITaskItem>();
+            var nonMatching = new List<ITaskItem>();
+
+            var options = new Options
+            {
+                AllowWindowsPaths = true,
+                Dot = true,
+                IgnoreCase = true
+            };
+
+            var matchers = Wildcards
+                .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(wildcard => new Minimatcher(wildcard.Trim(), options))
+                .ToList();
+
+            foreach (var item in Items)
+            {
+                if (matchers.Any(matcher => matcher.IsMatch(item.ItemSpec)) ||
+                    matchers.Any(matcher => matcher.IsMatch(item.GetMetadata("Fullpath"))))
+                    matching.Add(item);
+                else
+                    nonMatching.Add(item);
+            }
+
+            MatchingItems = matching.ToArray();
+            NonMatchingItems = nonMatching.ToArray();
+
+            return true;
+        }
+    }
+}
--- src/NuGetizer.Tasks/Extensions.cs
+++ src/NuGetizer.Tasks/Extensions.cs
@@ -139,7 +139,7 @@ namespace NuGetizer
         public static void LogErrorCode(this TaskLoggingHelper log, string code, string message, params object[] messageArgs) =>
             log.LogError(string.Empty, code, string.Empty, string.Empty, 0, 0, 0, 0, message, messageArgs);
 
-        public static void LogWarningCode(this TaskLoggingHelper log, string code, string file, string message, params object[] messageArgs) =>
-            log.LogWarning(string.Empty, code, string.Empty, file, 0, 0, 0, 0, message, messageArgs);
+        public static void LogWarningCode(this TaskLoggingHelper log, string code, string message, params object[] messageArgs) =>
+            log.LogWarning(string.Empty, code, string.Empty, string.Empty, 0, 0, 0, 0, message, messageArgs);
     }
 }
