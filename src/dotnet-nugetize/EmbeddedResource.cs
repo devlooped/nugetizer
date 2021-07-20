4bddb03c2d34759022579b19572d734aa85a3cea
Daniel Cazzulino <daniel@cazzulino.com>
2021-07-19 21:25:22 -0300

    Skip non-nugetized projects when nugetizer runs for solution
    Rather than failing ungracefully, just skip those project where we can safely detect nugetizer isn't installed.
    
    Fixes #107

---
+++ src/dotnet-nugetize/EmbeddedResource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+static class EmbeddedResource
+{
+    static readonly string baseDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+    public static string GetContent(string relativePath)
+    {
+        var filePath = Path.Combine(baseDir, Path.GetFileName(relativePath));
+        if (File.Exists(filePath))
+            return File.ReadAllText(filePath);
+
+        var baseName = Assembly.GetExecutingAssembly().GetName().Name;
+        var resourceName = relativePath
+            .TrimStart('.')
+            .Replace(Path.DirectorySeparatorChar, '.')
+            .Replace(Path.AltDirectorySeparatorChar, '.');
+
+        var manifestResourceName = Assembly.GetExecutingAssembly()
+            .GetManifestResourceNames().FirstOrDefault(x => x.EndsWith(resourceName));
+
+        if (string.IsNullOrEmpty(manifestResourceName))
+            throw new InvalidOperationException($"Did not find required resource ending in '{resourceName}' in assembly '{baseName}'.");
+
+        using var stream = Assembly.GetExecutingAssembly()
+            .GetManifestResourceStream(manifestResourceName);
+
+        if (stream == null)
+            throw new InvalidOperationException($"Did not find required resource '{manifestResourceName}' in assembly '{baseName}'.");
+
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+}
