d770fc96993cc2abd0879ab901c5e385c86e127a
Daniel Cazzulino <daniel@cazzulino.com>
2023-02-11 00:03:40 +0000

    Add SponsorLink to ensure ongoing development and maintenance
    See https://github.com/devlooped/SponsorLink and
    https://www.cazzulino.com/sponsorlink.html.

---
+++ src/CodeAnalysis/SponsorLinker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Devlooped.CredentialManager;
+
+[Generator]
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic, LanguageNames.FSharp)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base(SponsorLinkSettings.Create(
+        "devlooped", "NuGetizer",
+        version: new Version(ThisAssembly.Info.Version).ToString(2)
+#if DEBUG
+        , quietDays: 0
+#endif
+        ))
+    { }
+}
