910bc42375f2707e816a86f040a467e6f5f04e5b
Daniel Cazzulino <daniel@cazzulino.com>
2024-08-08 20:40:02 -0300

    Additional fix for --no-build in pack with transitive references
    Turns out that the .NET SDK lifts transitive project references as direct references (without any additional metadata), and this causes the second-level dependency from being built unexpectedly (see https://github.com/dotnet/sdk/pull/478 and https://github.com/dotnet/project-system/issues/199).
    
    Since we don't want to disrupt the IDE (BuildingInsideVisualStudio) and we only want to fix this for the very specific case of running from the CLI `dotnet pack --no-build`, we make the fix very constrained for that scenario. We check for `NoBuild` but ALSO for `_IsPacking`, which is passed by the `dotnet pack` command.
    
    This ensures minimal impact in all other scenarios, since we're essentially turning off a built-in behavior in the SDK that has explicit side-effects (by design and desirable) and we should preserve.
    
    #Fixes 501

---
+++ src/NuGetizer.Tests/given_transitive_projects.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace NuGetizer
+{
+    public class given_transitive_projects
+    {
+        readonly ITestOutputHelper output;
+
+        public given_transitive_projects(ITestOutputHelper output)
+        {
+            this.output = output;
+            using var disable = OpenBuildLogAttribute.Disable();
+
+            Builder.BuildScenario(nameof(given_transitive_projects), target: "Restore", projectName: "Project1")
+                .AssertSuccess(output);
+
+            Builder.BuildScenario(nameof(given_transitive_projects), target: "Build", projectName: "Project1")
+                .AssertSuccess(output);
+        }
+
+        [Fact]
+        public void when_pack_no_build_then_succeeds()
+        {
+            var result = Builder.BuildScenario(nameof(given_transitive_projects),
+                projectName: "Project1",
+                // These are the properties passed by dotnet pack --no-build
+                properties: new { NoBuild = "true", _IsPacking = "true" },
+                target: "GetPackageContents,Pack");
+
+            result.AssertSuccess(output);
+
+            Assert.True(result.BuildResult.HasResultsForTarget("GetPackageContents"));
+
+            var items = result.BuildResult.ResultsByTarget["GetPackageContents"];
+
+            Assert.Contains(items.Items, item => item.Matches(new
+            {
+                PackagePath = "lib/net8.0/Project1.dll"
+            }));
+
+            Assert.Contains(items.Items, item => item.Matches(new
+            {
+                PackagePath = "lib/net8.0/Project2.dll"
+            }));
+
+            Assert.Contains(items.Items, item => item.Matches(new
+            {
+                PackagePath = "lib/net8.0/Project3.dll"
+            }));
+        }
+    }
+}
